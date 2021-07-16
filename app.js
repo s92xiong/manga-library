272e5b2585afdd33170db2f15af4b97ab4da32b6
Sheuh Xiong <sheuhxiong@gmail.com>
2021-07-16 15:29:19 -0400

    Created author model

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = "mongodb+srv://sXiongdb:s92xiong-manga-library@cluster0.hh4be.mongodb.net/manga-library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
