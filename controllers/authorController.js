2cb0b1012137504e7a696ff10d95dda8d2dae3e7
Sheuh Xiong <sheuhxiong@gmail.com>
2021-07-20 17:41:53 -0400

    removed genre list in author detail pug file

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,8 @@
 const Author = require("../models/author");
 const Manga = require("../models/manga");
 const Genre = require("../models/genre");
-const Magazine = require("../models/magazine");
+
+const { body, validationResult } = require("express-validator");
 
 const async = require("async");
 
@@ -15,23 +16,12 @@ exports.author_list = function(req, res, next) {
 
 // Display detail page for a specific Author.
 exports.author_detail = function(req, res, next) {
-	// Things we want to display in the Author Detail:
-		// (1) All of the Author's Manga
-		// (2) All of the Magazines author is associated with (can be multiple)
-		// (3) Genres that author writes for
-		
 	async.parallel({
 		author: function(cb) { Author.findById(req.params.id).exec(cb) },
 		manga: function(cb) { Manga.find({ "author": req.params.id }).exec(cb) },
-		genre: function(cb) { Genre.find().sort([["name", "ascending"]]).exec(cb) },
 	}, (err, results) => {
 		if (err) return next(err);
-		return res.render("author_detail", { 
-			title: "Author", 
-			author: results.author,
-			manga_list: results.manga,
-			genre_list: results.genre,
-		});
+		return res.render("author_detail", { title: "Author", author: results.author, manga_list: results.manga });
 	});
 };
 
@@ -41,9 +31,29 @@ exports.author_create_get = function(req, res, next) {
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res, next) {
-	res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+	body("first_name").trim().isLength({ min: 1 }).escape().withMessage("First name must be specified.").isAlphanumeric().withMessage("First name has non-alphanumeric characters"),
+	body("family_name").trim().isLength({ min: 1 }).escape().withMessage("Family name must be specified.").isAlphanumeric().withMessage("Family name has non-alphanumeric characters"),
+	body("date_of_birth", "Invalid date of birth").optional({ checkFalsy: true }).isISO8601().toDate(),
+	body("date_of_death", "Invalid date of death").optional({ checkFalsy: true }).isISO8601().toDate(),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) return res.render("author_form", { title: "Create an Author", errors: errors.array() });
+
+		const author = new Author({
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death
+		});
+
+		author.save((err) => {
+			if (err) return next(err);
+			res.redirect(author.url);
+		});
+	}
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res, next) {
